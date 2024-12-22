b1397bfdb549a49d7de59e0d0efe1d57a779d2f8
Ramon Martinez Rodriguez <125287708+u1979261@users.noreply.github.com>
2024-12-22 14:17:19 +0100

    Final

--- Assets/TriggerDetector.cs
+++ Assets/TriggerDetector.cs
@@ -9,12 +9,30 @@ public class TriggerDetector : MonoBehaviour
     {
         if (other.CompareTag("Ball"))
         {
+            Debug.Log(personage.name);
+            float probability = GetProbabilityByName(personage.name);
             float randomChance = Random.Range(0f, 1f);
-            if (randomChance <= 0.2f)
+
+            if (randomChance <= probability)
             {
                 OnObjectTriggered?.Invoke();
                 Destroy(personage);
             }
+        }
+    }
+
+    private float GetProbabilityByName(string name)
+    {
+        switch (name)
+        {
+            case "Bear":
+                return 0.1f; // 50% probability
+            case "Mutante":
+                return 0.2f; // 80% probability
+            case "Reno":
+                return 0.3f; // 20% probability
+            default:
+                return 0.1f; // Default 10% probability
         }
     }
 }
